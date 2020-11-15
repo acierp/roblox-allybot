adf4196c8a75339ec206f809dd9bcd9a2c5af284
AcierP <61130011+AcierP@users.noreply.github.com>
2020-11-14 22:29:01 -0800

    Update allybot.py

--- allybot.py
+++ allybot.py
@@ -22,13 +22,12 @@ cookie = config['cookie']
 selfgroupid = config['selfgroupid']
 
 keywords = ['Military', 'Roleplay', 'Fan', 'Fun', 'Youtube', 'Games', 'Roblox', 'Productions', 'Hi', 'Noob', 'Raid', 'Group', 'difficult', 'easy', 'fun', 'funny', 'hard', 'friend', 'cool', 'fortnite', 'hotel', 'cafe', 'gamer', 'fall guys', 'minecraft', 'vibin', 'i love her..', 'shes my world..', '❤️', '😐', 'dm me please', 'hiiiii', "Coder", "Vegan", "Man", "Hacker", "Horse", "Bear", 'lol', 'X', 'famous', '!!!', '   ', 'isnot', 'me', 'help', 'FN', 'MINECRAFT', 'minecraft', 'fortnite', 'PUBG', 'rust', 'CSGO', 'csgo', ' hi there', ' dm me!', ''"Goat", "Goblin", "Learner", "Killer", "Woman", "Programmer", "Spy", "Stalker", "Spooderman", "Carrot", "Goat", "Quickscoper", "Quickscoper", "C#", "lol", 'funny', 'dead', 'TV', 'epic', '23', '656', '1234', '124', '1202', '🎃', '0', 'vie', 'via', 'cier', 'virtual', 'smh', 'pain', 'depress', 'sub', 'YT', 'DOE', ' Water', 'lis', 'hi', ' !', 's', 'vs', 'FUNNY', 'meme', 'guy', 'man', 'nigga', 'black', "Super", "Retarded", "Great", "Sexy", "Vegan", "Brave", 'X' "Shy", "Cool", 'YOUTUBE', 'YT', 'TTV', 'Twitch' "Poor", "Rich", "Fast", "Gummy", "Yummy", "Masked", "Unusual", "American", "Bisexual", "MLG", "Mlg", "lil", "Lil", "Bro", "Fortnite", "Red", "Ball", "Simp", ".", "$", "!", "al", "ban", "funny", 'epic', 'boi', 'bark', 'dy', 'rd', 'by', 'ac', 'icon', 'joes', 'linus', 'python', 'snake', 'liquid', 'pro', 'metro', 'nic', 'acier', 'ace', 'Nut', 'Ice', 'PS', 'pup', 'BRO', 'RAY', 'system', 'tim', 'theo', 'void', 'yellow', 'skia', 'Festive', 'Why', 'XD', 'homo', 'HA','Beauty','Twig','Cindy Lou Who','Tomcat','Stud','Loosetooth','Harry Potter','Rabbit','Gummi Bear','Tank','Fun Dip','Coke Zero','Raisin','Daria','Cowboy','Chump','Dragonfly','Headlights','Bossy','Big Nasty','Tough Guy','Duckling','Buckeye','Amour','Fatty','Smirk','Mini Me','Autumn','Cheesestick','Amigo','Salt','Diesel','Doctor','Foxy','Swiss Miss','Bumblebee','Midge','Captain','Diet Coke','Music Man','Belch','C-Dawg','Beast','Princess','Buck','Lovely','Prego','Freak','Bunny Rabbit','Carrot','Heisenberg','Cello','Homer','Coach','Cumulus','Gummy Pop','Winnie','Turkey','Bambino','Bubbles','Dorito','Captain Crunch','Pixie Stick','Filly Fally','Backbone','Silly Sally','Pinata','Buzz','Peppa Pig','Halfling','Hubby','Crumbles','Cutie Pie','Tootsie','Twiggy','Dummy','Cookie Dough','Muscles','Oompa Loompa','Chewbacca','Mini Skirt','Bumpkin','Boo Bug','Baby Maker','Cutie','Piglet','Piggy','Dragon','Bean','Beetle','Dimples','Mimi','Bug','Silly Gilly','Rapunzel','Admiral','Brown Sugar','Ringo','Amiga','Nerd','Guy','Junior','Cuddles','Twizzler','Biffle','Lefty','Ginger','Hot Sauce','Pookie','Fiesta','Fifi','Shrinkwrap','Tater','Fun Size','Baldie','Chili','Teeny','Bubba','Redbull','Chubs','Short Shorts','Mouse','Bebe','Dum Dum','Dino','Sugar','Fattykins','Bridge','Dottie','Creep','Herp Derp','Baby Bird','Cruella','Peppermint','Buttercup','Smoochie','Lover','Weirdo','Snoopy','Dulce','Con','Dirty Harry','Brutus','Weiner','Donuts','Braniac','Skinny Minny','Goose','Matey','Cottonball','Candycane','Chuckles','Grease','Barbie','Queenie','Papito','Boo Bear''Rosebud','Lil Mama','Dot','Bud','Honeybun','Figgy','Pork Chop','Angel','Skinny Jeans','Hulk','Miss Piggy','Lion','General','Frauline','Chickie','Senior','Giggles','Marge','Lil Girl','Gordo','Shorty']
-req = requests.Session()
 
 def tokenUpdater(cookie):
     cookies = {
         ".ROBLOSECURITY": cookie
     }
-    r = req.post("https://auth.roblox.com/v1/logout", cookies=cookies, headers={"connection":"keep-alive"})
+    r = requests.post("https://auth.roblox.com/v1/logout", cookies=cookies, headers={"connection":"keep-alive"})
     if r.status_code == 200 or r.status_code == 403:
         currenttoken.append(r.headers['x-csrf-token'])
         print('Successfully updated token')
@@ -45,7 +44,7 @@ def sendRequest(selfgroup, cookie, proxy, groupid):
     proxy = {
         "https": "https://" + next(ProxyPool)
     }
-    r = req.post(f'https://groups.roblox.com/v1/groups/{selfgroup}/relationships/allies/{groupid}', cookies=cookies, proxies=proxy, headers=headers)
+    r = requests.post(f'https://groups.roblox.com/v1/groups/{selfgroup}/relationships/allies/{groupid}', cookies=cookies, proxies=proxy, headers=headers)
     if r.status_code == 200:
         print(f'Successfully sent ally request to {groupid}!')
     elif r.status_code == 400:
@@ -68,7 +67,7 @@ def scrapeGroups(cookie, proxy, keyword, cursor=None):
         ".ROBLOSECURITY": cookie
     }
     if cursor == None:
-        r = req.get(
+        r = requests.get(
             f'https://groups.roblox.com/v1/groups/search?keyword={keyword}&limit=100', headers={"connection":"keep-alive"}, cookies=cookies, proxies=proxy).json()
         for breadchill in r['data']:
             squeakalusnoob = breadchill['id']
@@ -80,7 +79,7 @@ def scrapeGroups(cookie, proxy, keyword, cursor=None):
             except KeyError:
                 print('Finished checking all pages on a single thread.')
     else:
-        r = req.get(f'https://groups.roblox.com/v1/groups/search?keyword={keyword}&limit=100&cursor={cursor}', cookies=cookies, headers={"connection":"keep-alive"}, proxies=proxy).json()
+        r = requests.get(f'https://groups.roblox.com/v1/groups/search?keyword={keyword}&limit=100&cursor={cursor}', cookies=cookies, headers={"connection":"keep-alive"}, proxies=proxy).json()
         for breadchill in r['data']:
             squeakalusnoob = breadchill['id']
             print(f'Successfully scraped {squeakalusnoob}')
@@ -114,7 +113,7 @@ if "1" in choice:
         threading.Thread(target=scrapeGroups, args=[cookie, proxy, key]).start()
 elif "2" in choice:
     tokenUpdater(cookie)
-    for _ in range(500):
+    for _ in range(200):
         proxy = {
             "https": "https://" + next(ProxyPool)
         }
